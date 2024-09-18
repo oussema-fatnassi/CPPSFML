b366ca53826e4ab66fdd90f9c55b1e290fae60aa
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-09-18 11:24:29 +0200

    feat : get normal of bricks to calculate reflection abter bounce

--- src/Brick.cpp
+++ src/Brick.cpp
@@ -16,3 +16,27 @@ int Brick::getHealth() const {
 sf::FloatRect Brick::getBounds() const {
     return sprite.getGlobalBounds();
 }
+
+sf::Vector2f Brick::getNormal(const sf::Vector2f& collisionPoint) {
+    sf::FloatRect bounds = getBounds();
+
+    // Calculate distances to each side
+    float distanceToLeft = collisionPoint.x - bounds.left;
+    float distanceToRight = bounds.left + bounds.width - collisionPoint.x;
+    float distanceToTop = collisionPoint.y - bounds.top;
+    float distanceToBottom = bounds.top + bounds.height - collisionPoint.y;
+
+    // Find the smallest distance
+    float minDistance = std::min({distanceToLeft, distanceToRight, distanceToTop, distanceToBottom});
+
+    // Return the normal vector of the closest side
+    if (minDistance == distanceToLeft) {
+        return sf::Vector2f(-1.f, 0.f); // Left side
+    } else if (minDistance == distanceToRight) {
+        return sf::Vector2f(1.f, 0.f);  // Right side
+    } else if (minDistance == distanceToTop) {
+        return sf::Vector2f(0.f, -1.f); // Top side
+    } else {
+        return sf::Vector2f(0.f, 1.f);  // Bottom side
+    }
+}

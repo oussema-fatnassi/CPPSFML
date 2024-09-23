2b42a825a150c1737e773d83ef796f31be0f896d
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-09-23 10:25:24 +0200

    feat : add sound when shooting

--- include/Cannon.hpp
+++ include/Cannon.hpp
@@ -5,6 +5,7 @@
 #include "InputManager.hpp"
 #include "GameObject.hpp"
 #include "MathHelper.hpp"
+#include "SoundManager.hpp"
 #include "Bullet.hpp"
 #include <cmath>
 #include <vector>
@@ -12,7 +13,7 @@ using namespace std;
 
 class Cannon : public GameObject {
 public:
-    Cannon(sf::Vector2f position, sf::Vector2f dimension, const string& imagePath);
+    Cannon(sf::Vector2f position, sf::Vector2f dimension, const string& imagePath, SoundManager* soundManager);
     void render(sf::RenderWindow& window) override;
     void setRotation(float rotation);
     float getRotation() const;
@@ -27,6 +28,8 @@ private:
     sf::Vector2f determineBorderNormal(const sf::Vector2f& lineEnd, const sf::Vector2u& windowSize);
     sf::Vector2f reflectDirection(const sf::Vector2f& direction, const sf::Vector2f& normal);
     TrajectoryManager trajectoryManager;
+    SoundManager* soundManager;
+
 };
 
 #endif // CANNON_HPP
